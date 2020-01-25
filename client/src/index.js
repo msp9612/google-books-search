3cb05f961083b95ff51817b29d0233fb3dab402c
Matt Petrower <53028880+msp9612@users.noreply.github.com>
2020-01-24 23:28:10 -0500

    Add redux Provider wrapper and hot module reload

--- client/src/index.js
+++ client/src/index.js
@@ -1,10 +1,30 @@
+/* Dependencies */
+/* -------------------------------*/
+
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { render } from 'react-dom';
+import { Provider } from 'react-redux';
+import store from './redux/store';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+/* MAIN */
+/* -------------------------------*/
+
+// react-redux Provider wrapper
+const renderApp = () =>
+	render(
+		<Provider store={store}>
+			<App />
+		</Provider>,
+		document.getElementById('root')
+	);
+
+// Hot module reloading for app
+if (process.env.NODE_ENV !== 'production' && module.hot) {
+	module.hot.accept('./App', renderApp);
+}
+renderApp();
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
